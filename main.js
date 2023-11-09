216653c708ae667086ab2a4257303efe6c420381
Ice <gpreciousalyssa@gmail.com>
2023-11-08 21:30:06 -0600

    Play five rounds of Rock, Paper, Scissors in web console

--- main.js
+++ main.js
@@ -1,33 +1,67 @@
-console.log("hi")
-
-const move = ["rock", "paper", "scissors"]
-
 function getComputerChoice() {
+  const move = ["rock", "paper", "scissors"]
   i = Math.floor(Math.random() * move.length);
   return move[i]
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == computerSelection) {
-    return "It's a draw!"
-  }
-    
-  msgW = "You won! " + playerSelection + " beats " + computerSelection
-  msgL = "You lost! " + computerSelection + " beats " + playerSelection
-
   switch (playerSelection) {
     case "rock":
-      return computerSelection=="scissors" ? msgW : msgL;
+      return computerSelection=="scissors" ? 1 : 0;
     case "paper":
-      return computerSelection=="rock" ? msgW : msgL;
+      return computerSelection=="rock" ? 1 : 0;
     case "scissors":
-      return computerSelection=="paper" ? msgW : msgL;
+      return computerSelection=="paper" ? 1 : 0;
     default:
-      return "Invalid Entry"
+      return 2
   }
 }
 
-const playerSelection = prompt("Rock, Paper, Scissors?")
+var playerPoints = 0, computerPoints=0;
+function game() {
+  let playerSelection = prompt("Rock, Paper, Scissors?");
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = getComputerChoice();
 
-var res = playRound( playerSelection.toLowerCase(), getComputerChoice() )
-console.log(res)
+  let res = playRound(playerSelection, computerSelection)
+
+  if (playerSelection == computerSelection) {
+    return "It's a draw!"
+  }
+
+  switch (res) {
+    case 0:
+      computerPoints++;
+      let msgL = "You lost! " + computerSelection +
+                 " beats " + playerSelection
+      return msgL
+    
+    case 1:
+      playerPoints++;
+      let msgW = "You won! " + playerSelection +
+                 " beats " + computerSelection
+      return msgW
+    
+    case 2:
+      return "Invalid Input."                 
+  }
+}
+
+//Five Rounds
+for (let i=0; i<5; i++) {
+  let res = game()
+  console.log(res)
+  console.log("Computer: " + computerPoints + "; You: " + playerPoints)
+}
+
+
+const finalRes = playerPoints - computerPoints;
+if (finalRes==0) {
+    console.log("Final Result: It's a Draw!")
+}
+else if (finalRes>0) {
+  console.log("Final Result: You won!")
+}
+else {
+  console.log("Final Result: You lost!")
+}
